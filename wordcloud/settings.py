75dcb1dd6cb890236f27a1de06b91166f0cd54a7
yarnaid <yarnaid@gmail.com>
2015-04-07 17:41:16 +0300

    sekizai

--- wordcloud/settings.py
+++ wordcloud/settings.py
@@ -31,6 +31,7 @@ INTERNAL_IPS = ['*', '0.0.0.0', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = (
+    'grappelli.dashboard',
     'grappelli',
     'django.contrib.admin',
     'django.contrib.auth',
