36132b5f4cfb0f3eeb97ff0520c3dad74f3e056a
yarnaid <yarnaid@gmail.com>
2015-06-08 22:49:16 +0300

    chat

--- data_filter/templatetags/data_filter_tags.py
+++ data_filter/templatetags/data_filter_tags.py
@@ -6,10 +6,11 @@ register = template.Library()
 
 
 @register.inclusion_tag('data_filter/filter.html', takes_context=True)
-def data_filter(context):
+def data_filter(context, id_):
     jobs = models.Job.objects.all()
     context['job_ids'] = [j.id for j in jobs]
     context['job_numbers'] = [j.id for j in jobs]
+    context['id'] = id_
 
     # questions = models.Question.objects.filter(parent=jobs[0].id)
     context['questions_ids'] = []
--- vis_widget/static/vis_widget/js/helpers.js
+++ vis_widget/static/vis_widget/js/helpers.js
@@ -4,15 +4,23 @@ var show_menu = function(id_, div_id) {
     $(id_).toggleClass('hidden_menu');
     var full_height = $(div_id).height();
     var menu_pos = $(div_id).position();
-    $(id_).width('30%');
     $(id_).height(full_height);
     $(id_).css({
         position: 'absolute',
-        left: menu_pos.left,
-        top: 0
+        left: menu_pos.left + 1.5,
+        top: '1.5px'
     });
-    $(id_).animate({
-        width: '70%'
-    }, 1000);
+    var width = '0%';
+    var opacity = 0;
+    if (!$(id_).hasClass('hidden_menu')) {
+        width = '33%';
+        opacity = 1;
+    }
+    $(id_)
+    .animate({
+        width: width,
+        opacity: opacity
+        }, 150);
+
     return false;
 };
